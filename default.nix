cf1029904fe6ea77205fe168a463507b7e750e3e
Rune K. Svendsen <runesvend@gmail.com>
2023-04-04 19:22:48 +0200

    Enable overriding dependencies

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
+{ sources ? import ./nix/sources.nix {}
+, haskellNix ? import sources.haskellNix {}
+, pkgsSrc ? import haskellNix.sources.nixpkgs-2211 # nixpkgs-unstable
+, pkgs ? pkgsSrc (haskellNix.nixpkgsArgs // { })
+}:
 let
-  sources = import ./nix/sources.nix {};
-  haskellNix = import sources.haskellNix {};
-  pkgs = import
-    haskellNix.sources.nixpkgs-unstable
-    haskellNix.nixpkgsArgs;
-in pkgs.haskell-nix.project {
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "crypto-orderbook-db-app";
+  mkHackage = import sources.haskell-nix-extra-hackage { inherit pkgs; };
+
+  # Docs: https://github.com/ilyakooo0/haskell-nix-extra-hackage/blob/master/README.md#how-to-use-it
+  hsPkgs = pkgs.haskell-nix.cabalProject ({
     src = ./.;
-  };
-  compiler-nix-name = "ghc8107";
-}
+    index-state = "2021-11-22T00:00:00Z";
+    compiler-nix-name = "ghc8107";
+  });
+in
+hsPkgs
